f31f7d6527f8db82e49d9bb90674b16df39cc00d
XingGaoY <yangxg13@gmail.com>
2017-08-28 11:24:08 +0800

    Add output of parsed ip header

--- src/core/ip4.c
+++ src/core/ip4.c
@@ -1,4 +1,5 @@
 #include "ip4.h"
+#include "util.h"
 
 /**
  * This function is called by the network interface device driver when
@@ -15,5 +16,26 @@ void ip4_input(char *p){
   struct ip_hdr *iphdr;
 
   iphdr = (struct ip_hdr*)p;
-  printf("Received an IP datagram");
+  printf("Received an IP datagram:\n");
+
+  printf("version: %01x\nheader length: %01x\nTOS: %02x\ntotal length: %04x\nidentification: %04x\noffset: %04x\nTTL: %02x\nproto: %02x\nheader checksum: %04x\n", 
+							IPH_V(iphdr),
+							IPH_HL(iphdr),
+							IPH_TOS(iphdr),
+							IPH_LEN(iphdr),
+							IPH_ID(iphdr),
+							IPH_OFFSET(iphdr),
+							IPH_TTL(iphdr),
+							IPH_PROTO(iphdr),
+							IPH_CHKSUM(iphdr));
+  printf("src's ip:");
+  print_ip_addr(&iphdr->src);
+  printf("dst's ip:");
+  print_ip_addr(&iphdr->dest);
+
+  /* Here comes some check of header
+     we only do the checksum check now */
+
+
+  /* Send to upper layers */
 }
--- src/include/ip4.h
+++ src/include/ip4.h
@@ -3,6 +3,12 @@
 
 #include "ip4_addr.h"
 
+#define IP_PROTO_ICMP    1
+#define IP_PROTO_IGMP    2
+#define IP_PROTO_UDP     17
+#define IP_PROTO_UDPLITE 136
+#define IP_PROTO_TCP     6
+
 struct ip_hdr{
   uint8_t _v_hl;
   uint8_t _tos;
@@ -14,11 +20,19 @@ struct ip_hdr{
   uint16_t _chksum;
   struct ip4_addr src;
   struct ip4_addr dest;
+  char data[];
 };
 
 /* Macros to get struct ip_hdr fields: */
 #define IPH_V(hdr)  ((hdr)->_v_hl >> 4)
 #define IPH_HL(hdr) ((hdr)->_v_hl & 0x0f)
+#define IPH_TOS(hdr) ((hdr)->_tos)
+#define IPH_LEN(hdr) ((hdr)->_len)
+#define IPH_ID(hdr) ((hdr)->_id)
+#define IPH_OFFSET(hdr) ((hdr)->_offset)
+#define IPH_TTL(hdr) ((hdr)->_ttl)
+#define IPH_PROTO(hdr) ((hdr)->_proto)
+#define IPH_CHKSUM(hdr) ((hdr)->_chksum)
 
 /* Macros to set struct ip_hdr fields: */
 #define IPH_VHL_SET(hdr, v, hl) (hdr)->_v_hl = (u8_t)((((v) << 4) | (hl)))
