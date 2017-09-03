0876b25d2e3f04b458678afed963b7871e8fa7b0
XingGaoY <yangxg13@gmail.com>
2017-09-03 17:33:55 +0800

    The version of socket API with(socket, bind, recv) and udp_recv Add print of received message

--- src/app/udp_server.c
+++ src/app/udp_server.c
@@ -1,5 +1,6 @@
 #include "app.h"
 #include <stdio.h>
+#include <memory.h>
 
 void udp_server(){
   struct __sockaddr addr;
@@ -11,6 +12,13 @@ printf("UDP server...\n");
   raw_bind(sockfd, &addr);
   printf("UDP server binded...\n");
   while(1){
+    memset(buf, 0, 100);
     raw_recv(sockfd, buf, 100);
+    printf("=====\nMSG Received:\n");
+    for(int i=0; i<100; i++){
+      if(i%8 == 0) printf("\n");
+      printf("%02x", buf[i]);
+    }
+    printf("\n=====\n");
   }
 }
